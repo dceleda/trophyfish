425b6ce24748a30561f568be74c5e0e0f3a6140d
Daniel <wuzz@o2.pl>
2017-05-20 22:51:03 +0100

    top navbar styling

--- trophy-fish/src/app/app.module.ts
+++ trophy-fish/src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent
   ],
   imports: [
+    BsDropdownModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
