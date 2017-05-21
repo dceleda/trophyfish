ff3dcf357b5faab696644c57337ec3a1f004982f
Daniel <wuzz@o2.pl>
2017-05-21 13:44:33 +0100

    Web API Initial (with Logging)

---
+++ TrophyFishWebApi/TrophyFishApi/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Microsoft.ApplicationInsights.Extensibility;
+
+using NLog.Extensions.Logging;
+using NLog.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace TrophyFishApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            DisableTelemetryInDebug();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            env.ConfigureNLog("nlog.config");
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // Add framework services.
+            services.AddMvc();
+
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            
+
+            app.UseMvc();
+            //add NLog to ASP.NET Core
+            loggerFactory.AddNLog();
+
+            //add NLog.Web
+            app.AddNLogWeb();
+        }
+
+        [Conditional("DEBUG")]
+        private void DisableTelemetryInDebug()
+        {
+            TelemetryConfiguration.Active.DisableTelemetry = true;
+        }
+    }
+}
